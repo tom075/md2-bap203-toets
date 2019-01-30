718c26f80814ad17a8776026a612fdf9b2958163
Tom Folkers <24825@ma-web.nl>
2019-01-30 12:26:55 +0100

    error

--- resources/views/link-toevoegen.blade.php
+++ resources/views/link-toevoegen.blade.php
@@ -10,18 +10,33 @@
 
         <div class="form-group row">
             <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('title') }}</label>
+
             <div class="col-md-6">
                 <input id="title" type="text" class="form-control" name="title" required>
+                @if($errors->has('title'))
+                    <p class="text-danger">{{ $errors->first('title') }}</p>
+                @endif
             </div>
+
 
             <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('description') }}</label>
+
             <div class="col-md-6">
                 <textarea style="width: 100%; margin-top: 20px;" name="description" id="" cols="30" rows="10"></textarea>
+                @if($errors->has('description'))
+                    <p class="text-danger">{{ $errors->first('description') }}</p>
+                @endif
             </div>
 
+
             <label for="url" class="col-md-4 col-form-label text-md-right">{{ __('url') }}</label>
+
             <div class="col-md-6">
                 <input id="url" type="text" class="form-control" name="url" required>
+                @if($errors->has('url'))
+                    <p class="text-danger">{{ $errors->first('url') }}</p>
+                @endif
+
             </div>
 
 
